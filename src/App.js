77ff35eb2b71667d4b1b51e9765f735c3412c16c
Kevin Angulo Lezama <kevinjangulol@gmail.com>
2024-05-13 12:48:37 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 function App() {
-  return (
-    <Routes>
-      <Route path ="/" element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-      </Route>
-    </Routes>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
